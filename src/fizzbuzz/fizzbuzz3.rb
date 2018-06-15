a3e16533e0aa52652a18eba97315d9c64b2a0b17
yamazaki111 <baseballyuki46@gmail.com>
2018-06-15 21:38:37 +0900

    コメントを削除

--- src/fizzbuzz/fizzbuzz3.rb
+++ src/fizzbuzz/fizzbuzz3.rb
@@ -1,13 +1,3 @@
-# puts 1
-# puts 2
-# puts 3
-# puts 4
-# puts 5
-# puts 6
-# puts 7
-# puts 8
-# puts 9
-# puts 10
 def get_fizz_buzz(number)
     if (number % 3 == 0) && (number % 5 == 0)
         return "fizzbuzz"
@@ -21,19 +11,10 @@ def get_fizz_buzz(number)
 
 end
 
-
-
-
-
-
-
 puts "最大値を入力してください"
 max = gets
-
-
 
 for number in 1..max.to_i do
     fizz_buzz_text = get_fizz_buzz(number)
     puts fizz_buzz_text
 end
-
